647d8632af9f4637f84ddde3e40feda67c645912
JoseSalasCalderon <joseant2302@gmail.com>
2024-05-22 22:01:55 -0600

    Cambios generales y buscar Zonas por Concierto y Asiento

--- C07051_B92291_Tarea_IV_IF4101/Tarea4.BW/Interfaces/DA/IGestionarZonaDA.cs
+++ C07051_B92291_Tarea_IV_IF4101/Tarea4.BW/Interfaces/DA/IGestionarZonaDA.cs
@@ -10,5 +10,6 @@ namespace Tarea4.BW.Interfaces.DA
     public interface IGestionarZonaDA
     {
         public Task<IEnumerable<Zona>> buscarZonasPorConcierto(int idConcierto);
+        public Task<Zona> buscarZonaPorConciertoYAsiento(int idConcierto, int idAsiento);
     }
 }
